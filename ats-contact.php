1c23196dc12c3b1d53660893e5e418cd53300a5e
uidev-in <mail2loveseries@gmail.com>
2021-03-07 18:23:26 +0530

    Email format design

--- ats-contact.php
+++ ats-contact.php
@@ -19,35 +19,18 @@
        {
            $to = "sandyy1216@gmail.com";
            $subject="New Email From Website";
-           $message="
-           <html>
-            <head>
-            <title>ATS Email</title>
-            </head>
-            <body>
-            <table>
-            <tr>
-            <th>Firstname</th>
-            <th>Lastname</th>
-            <th>Email Id</th>
-            <th>Contact</th>
-            <th>Message</th>
-            </tr>
-            <tr>
-            <td>$FirstName</td>
-            <td>$LastName</td>
-            <td>$Email</td>
-            <td>$Contact</td>
-            <td>$Msg</td>
-            </tr>
-            </table>
-            </body>
-            </html>
-           ";
+           $message = '<html><body>';
+            $message .= '<table rules="all" style="border-color: #666;" cellpadding="10">';
+            $message .= "<tr style='background: #eee;'><td><strong>Name:</strong> </td><td>" .$FirstName. " ,".$LastName."</td></tr>";
+            $message .= "<tr style='background: #eee;'><td><strong>Email:</strong> </td><td>" .$Email. "</td></tr>";
+            $message .= "<tr style='background: #eee;'><td><strong>Contact:</strong> </td><td>" .$Contact. "</td></tr>";
+            $message .= "<tr style='background: #eee;'><td><strong>Message:</strong> </td><td>" .$Msg. "</td></tr>";
+            $message .= "</table>";
+            $message .= "</body></html>";
            
            // Always set content-type when sending HTML email
-            $headers = "MIME-Version: 1.0" . "\r\n";
-            $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+            $headers .= "MIME-Version: 1.0\r\n";
+            $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
            if(mail($to,$subject,$message,$headers))
            {
